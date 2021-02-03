3e5c70e6cbc0f329fba81158d27dd6a2d51ac0d7
Engin <enginsubasi@gmail.com>
2021-02-03 20:58:34 +0300

    +RTC

---
+++ Projects/09-RTC/TestFirstProject/Core/Inc/main.h
@@ -0,0 +1,90 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ACC_CS_Pin GPIO_PIN_3
+#define ACC_CS_GPIO_Port GPIOE
+#define USER_BUTTON_Pin GPIO_PIN_0
+#define USER_BUTTON_GPIO_Port GPIOA
+#define PWM_GREEN_Pin GPIO_PIN_12
+#define PWM_GREEN_GPIO_Port GPIOD
+#define PWM_ORANGE_Pin GPIO_PIN_13
+#define PWM_ORANGE_GPIO_Port GPIOD
+#define PWM_RED_Pin GPIO_PIN_14
+#define PWM_RED_GPIO_Port GPIOD
+#define PWM_BLUE_Pin GPIO_PIN_15
+#define PWM_BLUE_GPIO_Port GPIOD
+#define AUD_RST_Pin GPIO_PIN_4
+#define AUD_RST_GPIO_Port GPIOD
+#define ACC_INT_1_Pin GPIO_PIN_0
+#define ACC_INT_1_GPIO_Port GPIOE
+#define ACC_INT_2_Pin GPIO_PIN_1
+#define ACC_INT_2_GPIO_Port GPIOE
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
